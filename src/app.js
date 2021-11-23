feb054672f19d671679e8059ffb8e013a4345b2d
Richard Ruppel <rmruppelus.ibm.com@Richards-MacBook-Air.local>
2021-11-23 16:02:26 -0500

    Correct port number

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.port || 3000  //process.env.port for Heroku
-console.log(port)
+const port = process.env.PORT || 3000  //process.env.port for Heroku
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
